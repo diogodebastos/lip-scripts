093d3c3efbdef8884a7f5e7e7d83f57dae8d29cc
diogodebastos <diogodebastos18@gmail.com>
2018-12-06 16:52:50 +0000

    sub_arrays

--- statusSamplesFromGrid.sh
+++ statusSamplesFromGrid.sh
@@ -3,5 +3,5 @@ source allSamplesFromGrid.sh
 
 for sample in "${samples[@]}"; do
  echo -e "\033[0;31m$sample\033[0m"
- ls -d ${GSTORE}/${sample}*/18*/*/heppyOutput_* | wc && ls -d ${GSTORE}/${sample}/Chunk* | wc
+ ls -d ${GSTORE}/${sample}*/18*/*/heppyOutput_* | wc -l && ls -d ${GSTORE}/${sample}/Chunk* | wc -l
 done
