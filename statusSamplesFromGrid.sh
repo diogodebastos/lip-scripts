284313fcc218bad05c3469d561bf16ca3d3e266d
diogodebastos <diogodebastos18@gmail.com>
2018-12-13 13:43:15 +0000

    better status of transfered samples

--- statusSamplesFromGrid.sh
+++ statusSamplesFromGrid.sh
@@ -3,5 +3,5 @@ source allSamplesFromGrid.sh
 
 for sample in "${samples[@]}"; do
  echo -e "\033[0;31m$sample\033[0m"
- ls -d ${GSTORE}/${sample}*/18*/*/heppyOutput_* | wc -l && ls -d ${GSTORE}/${sample}/Chunk* | wc -l
+ ls -d ${GSTORE}/${sample}*/18*/*/heppyOutput_* | wc -l && ls -d ${GSTORE}/${sample}/Chunk*/treeProducerStop4Body/tree.root | wc -l
 done
