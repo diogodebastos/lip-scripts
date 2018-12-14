97c5162043d74c54ea366b5ebec1d7450fe53e40
Diogo de Bastos <diogodebastos18@gmail.com>
2018-12-14 13:38:04 +0000

    minor fix

--- statusSamplesFromGrid.sh
+++ statusSamplesFromGrid.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source allSamplesFromGrid.sh
 
-for sample in "${TEST[@]}"; do
- heppy_out=$(ls -d ${GSTORE}/${sample}*/18*/*/heppyOutput_* | wc -l)
+for sample in "${samples[@]}"; do
+ heppy_out=$(ls -d ${GSTORE}/${sample}/18*/0*/heppyOutput_* | wc -l)
  chunk=$(ls -d ${GSTORE}/${sample}/Chunk*/treeProducerStop4Body/tree.root | wc -l)
  dif=$(( $heppy_out - $chunk))
  if [ $dif = 0 ]; then
   echo -e "\033[0;32m$sample\033[0m"
  else
   echo -e "\033[0;31m$sample\033[0m"
-  if [[ $dif < 10 ]]; then
+  if [[ $dif < 30 ]]; then
    echo "Bad Chunks:"
    for j in $(seq 1 $heppy_out); do
     dir=${GSTORE}/${sample}/Chunk${j}/treeProducerStop4Body/tree.root
